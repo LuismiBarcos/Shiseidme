c59a8f252cfb4731a05f30616392749d27461707
Victor Galan <victor.galan@liferay.com>
2018-07-03 11:49:26 +0200

    Add user view to display user in the asset display

---
+++ Shiseidme/Themes/UserView.swift
@@ -0,0 +1,54 @@
+//
+//  UserView.swift
+//  Shiseidme
+//
+//  Created by Victor Galan on 02/07/2018.
+//  Copyright © 2018 Luis Miguel Barco. All rights reserved.
+//
+
+import UIKit
+import LiferayScreens
+
+class UserView: UIView {
+
+	@IBOutlet weak var userPortraitScreenlet: UserPortraitScreenlet?
+	@IBOutlet weak var usernameLabel: UILabel?
+
+	var user: User? {
+		didSet {
+			userPortraitScreenlet?.load(userId: user!.userId)
+			usernameLabel?.text = "\(user!.firstName) \(user!.lastName)"
+		}
+	}
+
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		setup()
+	}
+
+	required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+		setup()
+	}
+
+	fileprivate func setup() {
+		let nib = Bundle.main.loadNibNamed("UserView", owner: self, options: nil)
+		if let view = nib?.last as? UIView {
+			self.addSubview(view)
+
+			view.translatesAutoresizingMaskIntoConstraints = false
+
+			//Pin all edges from Screenlet View to the Screenlet's edges
+			let top = NSLayoutConstraint(item: view, attribute: .top, relatedBy: .equal,
+										 toItem: self, attribute: .top, multiplier: 1, constant: 0)
+			let bottom = NSLayoutConstraint(item: view, attribute: .bottom, relatedBy: .equal,
+											toItem: self, attribute: .bottom, multiplier: 1, constant: 0)
+			let leading = NSLayoutConstraint(item: view, attribute: .leading, relatedBy: .equal,
+											 toItem: self, attribute: .leading, multiplier: 1, constant: 0)
+			let trailing = NSLayoutConstraint(item: view, attribute: .trailing, relatedBy: .equal,
+											  toItem: self, attribute: .trailing, multiplier: 1, constant: 0)
+
+			NSLayoutConstraint.activate([top, bottom, leading, trailing])
+		}
+	}
+}
