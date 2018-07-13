3fe427acffac70f0c5c80b9ed7bed914351f0ef4
Luis Miguel Barco <luismiguelbarco@LFRW0164.liferay.org.es>
2018-07-13 09:49:55 +0200

    Add take picture feature

--- Shiseidme/Extensions/UIViewControllerExtensions.swift
+++ Shiseidme/Extensions/UIViewControllerExtensions.swift
@@ -8,9 +8,8 @@
 
 import Foundation
 import UIKit
-import iOSPhotoEditor
 
-extension UIViewController:  UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+extension UIViewController {
     func showAlert(title: String) {
         let alert = UIAlertController.init(title: title, message: nil, preferredStyle: .alert)
         let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
@@ -24,36 +23,5 @@ extension UIViewController:  UIImagePickerControllerDelegate, UINavigationContro
         let viewController = storyBoard.instantiateViewController(withIdentifier: screenId) as! T
         //        self.present(viewController as! UIViewController, animated: true, completion: nil)
         self.navigationController?.pushViewController(viewController as! UIViewController, animated: true)
-    }
-    
-    public func imagePickerController(_ picker: UIImagePickerController,
-                               didFinishPickingMediaWithInfo info: [String : Any]) {
-        
-        guard let image = info[UIImagePickerControllerOriginalImage] as? UIImage else {
-            picker.dismiss(animated: true, completion: nil)
-            return
-        }
-        picker.dismiss(animated: true, completion: nil)
-        
-        
-        let photoEditor = PhotoEditorViewController(nibName:"PhotoEditorViewController",bundle: Bundle(for: PhotoEditorViewController.self))
-        photoEditor.photoEditorDelegate = self as? PhotoEditorDelegate
-        photoEditor.image = image
-        //Colors for drawing and Text, If not set default values will be used
-        //photoEditor.colors = [.red, .blue, .green]
-        
-        //Stickers that the user will choose from to add on the image
-        for i in 0...10 {
-            photoEditor.stickers.append(UIImage(named: i.description )!)
-        }
-        
-        //To hide controls - array of enum control
-        //photoEditor.hiddenControls = [.crop, .draw, .share]
-        
-        present(photoEditor, animated: true, completion: nil)
-    }
-    
-    public func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        dismiss(animated: true, completion: nil)
     }
 }
