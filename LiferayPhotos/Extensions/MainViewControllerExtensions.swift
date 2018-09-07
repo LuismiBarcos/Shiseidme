f51e6ead99fb99abf38f4201c42f8a64e108f7a4
Victor Galan <victor.galan@liferay.com>
2018-09-07 10:53:27 +0200

    Renaming

--- LiferayPhotos/Extensions/MainViewControllerExtensions.swift
+++ LiferayPhotos/Extensions/MainViewControllerExtensions.swift
@@ -1,10 +1,16 @@
-//
-//  MainViewControllerExtensions.swift
-//  Shiseidme
-//
-//  Created by Luis Miguel Barco on 12/07/2018.
-//  Copyright © 2018 Luis Miguel Barco. All rights reserved.
-//
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
 
 import Foundation
 import iOSPhotoEditor
